e471e9de3debe14a985e096760f9c688d9636bcd
kekes-dotcom <wisnarikeerpl@gmail.com>
2026-08-05 12:40:06 +0700

    Add files via upload

---
+++ bab3.py
@@ -0,0 +1,17 @@
+nama = "rike"
+print("nama : ", nama)
+umur = 15
+print("umur : ", umur)
+tinggi = 147
+print("tinggi :", tinggi, "cm")
+lulus = False
+print("lulus :", lulus)
+
+nama_produk : "sepatu lari"
+print("nama produk:", nama_produk)
+stok_tersedia : 15
+print("stok tersedia:", stok_tersedia)
+harga ($) : 249.5
+print("harga ($):", harga ($))
+tersedia di toko : True
+print("tersedia di toko:", tersedia di toko)
