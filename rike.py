a72498f883ba67862f77f770f9869f61dca39871
kekes-dotcom <wisnarikeerpl@gmail.com>
2026-08-12 08:04:27 +0700

    Delete rike.py

--- rike.py
+++
@@ -1,7 +0,0 @@
-# ini adalah komentar - tidak dijalankan oleh python
-# progam: salam perkenalan
-
-print("halo,dunia")               #Menampilkan teks
-print("nama saya: rike")            #Setiap print () = satu baris output
-print("versi: 3.x")               # Tanda kutip bisa ganda atau tunggal
- 
