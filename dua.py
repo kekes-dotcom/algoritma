75bc6d8a8ff0b84ce8a46d4e440980954eb5fb43
kekes-dotcom <wisnarikeerpl@gmail.com>
2026-08-26 07:42:48 +0700

    Add files via upload

---
+++ dua.py
@@ -0,0 +1,19 @@
+# progam pertamaku
+# nama: [rike]
+# kelas: X RPL 
+
+print("------------------------------------------")
+print("       SELAMAT DATANG DI PYTHON!")
+print("------------------------------------------")
+print()
+print("progam ini dibuat oleh:", "[rike]")
+print("sekolah: SMK TJP TUBAN")
+print("Tahun    :",    2024)
+
+print (10 + 5) 
+print (20 - 8) 
+print (4 * 8) 
+print (15 / 4) 
+print (15 // 4) 
+print (15 % 4) 
+print (2 ** 8) 
