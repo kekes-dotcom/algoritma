bea8270021eacde51a86c3dc0be2cfb1abc8f125
rikesa.com <wisnarikeerpl@gmail.com>
2026-09-02 12:37:25 +0700

    Add files via upload

---
+++ looping&while_loop.py
@@ -0,0 +1,24 @@
+# Contoh lengkap penggunaan range ()
+print("Genap 0-10:", end=" ")
+for i in range(0, 11, 2):
+    print(i; end=" ")
+# Output: 0 2 4 6 8 10
+
+print ()
+print("Hitung mundur:"< end=" ")
+for i in range(5, 0, -1):
+    print(i, end=" ")
+# Output: 5 4 3 2 1
+
+
+# Sintaks while loop
+while kondisi:
+    # blok kode yang diulang
+    # pastikan ada yang mengubah kondisi!
+    
+# Contoh: Hitung mundur dari 5
+n = 5
+while n > 0:
+    print("Hitung mundur:", n)
+    n -= 1    # n = n - 1  (kondisi akhirnya False)
+print("MULAI!")pppp
